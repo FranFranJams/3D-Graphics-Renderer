cd318e2eb8390a772fa240ce969733c033337f43
FranFranJams <SuperNewComputer16@outlook.com>
2021-10-10 15:20:23 +0100

    Added FPS counter & made Display black

--- 3DRenderer/src/renderer/Display.java
+++ 3DRenderer/src/renderer/Display.java
@@ -1,5 +1,9 @@
+package renderer;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -56,9 +60,49 @@ public class Display extends Canvas implements Runnable {
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60;
+		double delta = 0;
+		int frames = 0;
+			
 		
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				this.frame.setTitle(title + " | " + frames + " fps");
+				frames = 0;
+			}	
+		}
+		stop();
 	}
 	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH * 2, HEIGHT * 2);
+		
+		g.dispose();
+		bs.show();
+	}
 	
+	private void update() {
+		
+	}
 }
